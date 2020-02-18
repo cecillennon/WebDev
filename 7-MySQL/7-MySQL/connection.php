5e43edec0d6bde7698b0efa47d9d73b760a5a5fd
charles rummel <charles.g.rummel@gmail.com>
2020-02-18 13:11:21 -0600

    Add files via upload

---
+++ 7-MySQL/7-MySQL/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+	$link = mysqli_connect("shareddb1d.hosting.stackcp.net", "secretdi-3231838b", "Squirrel96!", "secretdi-3231838b");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        } 
+		
+?>
---
+++ 7-MySQL/7-MySQL/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  <head>
+    
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  
+	<style type="text/css">
+	
+		.container {
+			text-align: center;		
+			width: 400px; 
+		}
+		
+		#homePageContainer{
+			margin-top: 150px;
+		}
+		
+		#containerLoggedInPage{
+			margin-top: 60px;
+		}
+		
+		html { 
+			background: url(background.jpg) no-repeat center center fixed; 
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+		}
+		
+		body{
+			background: none;
+			color: white;
+		}
+		
+		#logInForm{
+			display: none;
+		}
+		
+		.toggleForms{
+			font-weight: bold;
+			
+		}
+		
+		#navTitle{
+			color: white;
+		}
+		
+		#diary{
+			width: 80%;
+			height: 100%;
+		}
+	
+	</style>
+	
+  </head>
+  
+  <body>
